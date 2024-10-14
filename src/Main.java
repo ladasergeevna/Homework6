b7bb068fd2d0d66008e3f23e40c9463a47630b66
user <ladasergeevna@mail.ru>
2024-10-14 16:56:47 +0900

    All tasks done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,95 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Task1
+        System.out.println("Task1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
+
+        //Task2
+        System.out.println();
+        System.out.println("Task2");
+        for (int j = 10; j >= 1; j--) {
+            System.out.println(j);
+        }
+
+        //Task3
+        System.out.println();
+        System.out.println("Task3");
+        for (int a = 0; a <= 17; a = a + 2) {
+            System.out.println(a);
+    }
+
+        //Task4
+        System.out.println();
+        System.out.println("Task4");
+        for (int b = 10; b >= -10; b--) {
+            System.out.println(b);
+        }
+
+        //Task5_1
+        System.out.println();
+        System.out.println("Task5_1");
+        for (int y = 1904; y <= 2096; y = y + 4) {
+            System.out.println(y + " год является високосным");
+        }
+
+        //Task5_2
+        System.out.println();
+        System.out.println("Task5_2");
+        for (int y = 1904; y <= 2096; y = y + 4) {
+            if ((y >= 1904 && (y % 4 == 0 && y % 100 != 0) || y % 400 == 0)) {
+                System.out.println(y + " год является високосным");
+            } else {
+                System.out.println(y + " год - не високосный");
+            }
+        }
+
+        //Task6
+        System.out.println();
+        System.out.println("Task6");
+        for (int z = 7; z <= 98; z = z + 7) {
+            System.out.println(z);
+        }
+
+        //Task7
+        System.out.println();
+        System.out.println("Task7");
+        for (int d = 1; d <= 512; d = d * 2) {
+            System.out.println(d);
+        }
+
+        //Task8
+        System.out.println();
+        System.out.println("Task8");
+        int deposit = 29000;
+        int total = 0;
+        for (int c = 1; c <= 12; c++) {
+            total = total + deposit;
+            System.out.println("Месяц " + c + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("За год сумма накоплений будет " + total + " рублей");
+
+        //Task9
+        System.out.println();
+        System.out.println("Task9");
+        float depositForPercent = 29000F;
+        float totalForPercent = 0F;
+        for (int c = 1; c <= 12; c++) {
+            totalForPercent = totalForPercent + totalForPercent/100;
+            totalForPercent = totalForPercent + depositForPercent;
+            System.out.println("Месяц " + c + ", сумма накоплений равна " + totalForPercent + " рублей");
+        }
+        System.out.println("За год сумма накоплений будет " + totalForPercent + " рублей");
+
+        //Task10
+        System.out.println();
+        System.out.println("Task10");
+        int firstNumber = 2;
+        for (int t = 1; t <= 10; t++) {
+            System.out.println(firstNumber + " * " + t + " = " + t * firstNumber);
+        }
+
     }
 }
